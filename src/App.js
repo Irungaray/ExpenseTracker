dfa8ca098054bc31788d2e736bd73e06e3c8086b
Irungaray <LionelIrungaray@GMail.com>
2020-09-08 10:19:23 -0300

    Añadidos los estilos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import { Header } from './components/Header';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello World
+      <Header />
     </div>
   );
 }
