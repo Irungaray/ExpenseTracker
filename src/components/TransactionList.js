75b148db86b46b0dd8c5bbe6ddc0eb6dc49ad235
Irungaray <LionelIrungaray@GMail.com>
2020-09-08 20:08:22 -0300

    Handled Balance

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react';
+
+import { GlobalContext } from '../context/GlobalState';
+
+import { Transaction } from './Transaction';
 
 export const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext);
+
     return (
         <>
             <h3>History</h3>
             <ul className="list">
-                <li className="minus">
-                Cash <span>-$400</span><button className="delete-btn">x</button>
-                </li>
+                {transactions.map(transaction => (
+                   <Transaction
+                        transaction={transaction}
+                        key={transaction.id}
+                    />
+                    // Passing 'transaction' as a prop,
+                    // which comes from the map function,
+                    // which comes from the GlobalContext
+                    // so for each one it renders a Transaction component passing a Prop
+                    // and because it's a key, it needs a unique Id
+                ))}
             </ul>
         </>
     )
