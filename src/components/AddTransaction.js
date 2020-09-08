ec512c6c25ec57662fd45b058573d2f5dce1fa8f
Irungaray <LionelIrungaray@GMail.com>
2020-09-08 11:05:09 -0300

    Added Components

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -10,14 +10,24 @@ export const AddTransaction = () => {
             <form> for
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
-                    <input type="text" value={} placeholder="Enter text..." />
+                    <input
+                        type="text"
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
+                        placeholder="Enter text..."
+                    />
                 </div>
                 <div className="form-control">
                     <label htmlFor="amount">
                         Amount <br />
                         (negative - expense, positive - income)
                     </label>
-                    <input type="number" placeholder="Enter amount..." />
+                    <input
+                        type="number"
+                        value={amount}
+                        onChange={(e) => setAmount(e.target.value)}
+                        placeholder="Enter amount..."
+                    />
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
