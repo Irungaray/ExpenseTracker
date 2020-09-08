1c05e583673fa5f2682c5ec4ec79c20d150c4b84
Irungaray <LionelIrungaray@GMail.com>
2020-09-08 20:40:11 -0300

    Handled Delete Transaction

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
+
     const sign = transaction.amount < 0 ? '-' : '+';
 
     return (
@@ -9,7 +13,13 @@ export const Transaction = ({ transaction }) => {
             <span>
                 {sign}${Math.abs(transaction.amount)}
             </span>
-            <button className="delete-btn">x</button>
+
+            <button
+                className="delete-btn"
+                onClick={() => deleteTransaction(transaction.id)}
+            >
+                x
+            </button>
         </li>
     )
 }
