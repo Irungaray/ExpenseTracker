3095b2ae67f2b84843f30bb606668d1f8dba6555
Irungaray <LionelIrungaray@GMail.com>
2020-09-08 22:05:15 -0300

    Updates

---
+++ precache-manifest.9d4bd812d4afb9d6505674bf5cb2893d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4265a179e654ca1a0b812f44a241096",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "b5dc56f3fd0ad2726a2c",
+    "url": "/ExpenseTracker/static/css/main.61327df0.chunk.css"
+  },
+  {
+    "revision": "64629b05b432c44e1bd6",
+    "url": "/ExpenseTracker/static/js/2.4599387a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ExpenseTracker/static/js/2.4599387a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5dc56f3fd0ad2726a2c",
+    "url": "/ExpenseTracker/static/js/main.19971f57.chunk.js"
+  },
+  {
+    "revision": "bce0d28d436fe3899340",
+    "url": "/ExpenseTracker/static/js/runtime-main.a7edbd44.js"
+  }
+]);
